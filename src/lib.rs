d5362bf29b3b982e99cf8b3f4fd12787d2d1d948
Nathan LeRoy <NLeRoy917@gmail.com>
2024-03-24 19:53:07 -0400

    work on neighbor distances

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,16 @@ pub fn calc_neighnor_distances(region_set: &mut RegionSet) -> Result<Vec<u32>> {
 
     // iterate over all chromosomes
     for chr in region_set.iter_choms() {
+        // if there is only one region on the chromosome, skip it, can't calculate distance between one region
+        if region_set.iter_regions(chr).count() < 2 {
+            continue;
+        }
         for i in 0..region_set.iter_regions(chr).count() - 1 {
             let region = region_set.iter_regions(chr).nth(i).unwrap();
             let next_region = region_set.iter_regions(chr).nth(i + 1).unwrap();
 
             let distance = next_region.start - region.end;
+
             distances.push(distance);
             
         } 
@@ -28,9 +33,7 @@ pub fn calc_neighnor_distances(region_set: &mut RegionSet) -> Result<Vec<u32>> {
     Ok(distances)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    
+pub mod prelude {
+    pub use super::models::{Region, RegionSet};
+    pub use super::calc_neighnor_distances;
 }
---
+++ tests/tests.rs
@@ -0,0 +1,41 @@
+use gdrs::prelude::*;
+use rstest::*;
+
+use std::path::Path;
+
+mod tests {
+    use super::*;
+
+    // #[fixture]
+    // fn path_to_bed() -> Path {
+    //     Path::new("tests/test.bed")
+    // }
+
+    // #[fixture]
+    // fn region_set() -> RegionSet {
+    //     let path_to_bed = Path::new("tests/test.bed");
+    //     RegionSet::from_bed_file(path_to_bed).unwrap()
+    // }
+
+    #[rstest]
+    fn test_calc_neighnor_distances() {
+        let mut region_set = RegionSet::from_bed_file(Path::new("tests/data/test.bed")).unwrap();
+        let actual = [
+            202_684_741,
+            32_120_636,
+            3_824_685,
+            43_450_165,
+            714_607,
+            105_011_571
+        ];
+
+        let distances = calc_neighnor_distances(&mut region_set).unwrap();
+        
+        // make sure that the distances are the same, doesnt matter in which order
+        for distance in distances {
+            assert!(actual.contains(&distance));
+        }
+        
+    }
+    
+}

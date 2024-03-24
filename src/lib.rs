957e0a4107472114182329f381b04ac02dd6f3e8
Nathan LeRoy <NLeRoy917@gmail.com>
2024-03-24 18:14:28 -0400

    initialize

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod models;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    
+}
---
+++ src/models.rs
@@ -0,0 +1,9 @@
+pub struct Region {
+    chr: String,
+    start: u32,
+    end: u32
+}
+
+pub struct BedFile {
+
+}

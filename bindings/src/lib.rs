ca48da07dafb6ef223b10a3ebc01454c5016810e
Nathan LeRoy <NLeRoy917@gmail.com>
2024-10-22 11:24:15 -0400

    fix?

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -4,7 +4,7 @@ mod models;
 mod tools;
 
 #[pymodule]
-fn gdrs(_py: Python, m: &PyModule) -> PyResult<()> {
+fn gdrs(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<models::PyGenomeAssembly>()?;
     m.add_function(wrap_pyfunction!(tools::py_calc_widths, m)?)?;
     m.add_function(wrap_pyfunction!(tools::py_calc_gc_content, m)?)?;
